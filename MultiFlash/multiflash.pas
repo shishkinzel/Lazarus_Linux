254a290441f2c09d2ef06730be8b61dfd5a8dc90
shishkinzel <shishkin_angtel@rambler.ru>
2022-10-16 20:33:50 +0300

    Первый проект на линукс, MultiFlash

---
+++ MultiFlash/multiflash.pas
@@ -0,0 +1,26 @@
+unit MultiFlash;
+
+{$mode objfpc}{$H+}
+
+interface
+
+uses
+  Classes, SysUtils, Forms, Controls, Graphics, Dialogs;
+
+type
+  TfrmMultiFlash = class(TForm)
+  private
+
+  public
+
+  end;
+
+var
+  frmMultiFlash: TfrmMultiFlash;
+
+implementation
+
+{$R *.lfm}
+
+end.
+
